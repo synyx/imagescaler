5a1063707916bb4514ed33cdf508425f0ac280f9
Robin de Silva Jayasinghe <jayasinghe@synyx.de>
2019-07-05 23:20:37 +0200

    move config stuff to its own file

--- imagescaler.go
+++ imagescaler.go
@@ -3,27 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+
 	"time"
 
-	"github.com/spf13/viper"
 	"github.com/streadway/amqp"
 )
-
-type rabbitConf struct {
-	hostname              string
-	port                  int
-	username              string
-	password              string
-	filename              string
-	imageExchange         string
-	imageUpdateQueue      string
-	imageUpdateRoutingKey string
-	timeout               time.Duration
-	minioURL              string
-	minioAccessKey        string
-	minioSecret           string
-	minioBucketName       string
-}
 
 func main() {
 	config := readConfig()
@@ -33,44 +17,6 @@ func main() {
 	ScaleImage(nil, THUMBNAIL)
 
 	log.Print("hallo")
-}
-func readConfig() rabbitConf {
-	viper.SetConfigFile("config.properties")
-	viper.SetConfigType("properties")
-
-	//default values suitable for vanilla rabbitmq docker container
-	viper.SetDefault("rabbitmq.hostname", "localhost")
-	viper.SetDefault("rabbitmq.port", "5672")
-	viper.SetDefault("rabbitmq.username", "guest")
-	viper.SetDefault("rabbitmq.password", "guest")
-	viper.SetDefault("rabbitmq.timeout", "5s")
-	viper.SetDefault("rabbitmq.image.exchange", "image.event")
-
-	//default values suitable for min.io docker container
-	viper.SetDefault("minio.url", "http://localhost:9000")
-	viper.SetDefault("minio.accesskey", "admin")
-	viper.SetDefault("minio.secret", "secret")
-	viper.SetDefault("minio.bucketname", "testbucket")
-
-	//load config
-	confErr := viper.ReadInConfig()
-	logOnError(confErr, "No configuration file loaded - using defaults")
-
-	return rabbitConf{
-		hostname:              viper.GetString("rabbitmq.hostname"),
-		port:                  viper.GetInt("rabbitmq.port"),
-		username:              viper.GetString("rabbitmq.username"),
-		password:              viper.GetString("rabbitmq.password"),
-		timeout:               viper.GetDuration("rabbitmq.timeout"),
-		filename:              viper.GetString("filename"),
-		imageExchange:         viper.GetString("rabbitmq.image.exchange"),
-		imageUpdateQueue:      viper.GetString("rabbitmq.image.udpate.queue"),
-		imageUpdateRoutingKey: viper.GetString("rabbitmq.image.update.routingkey"),
-		minioURL:              viper.GetString("minio.url"),
-		minioAccessKey:        viper.GetString("minio.accesskey"),
-		minioSecret:           viper.GetString("minio.secret"),
-		minioBucketName:       viper.GetString("minio.bucketname"),
-	}
 }
 
 func connectRabbit(conf rabbitConf) *amqp.Connection {
