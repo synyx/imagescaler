771d74906322c1914a6c6ada2f336aefd028ef7b
Robin de Silva Jayasinghe <jayasinghe@synyx.de>
2019-07-31 21:55:40 +0200

    write image metadata to imageUpdate
    also generate the relevant data in the uploader func.

--- go.mod
+++ go.mod
@@ -2,6 +2,7 @@ module imagescaler
 
 require (
 	github.com/minio/minio-go/v6 v6.0.33
+	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d
 	github.com/spf13/viper v1.3.2
 	github.com/streadway/amqp v0.0.0-20190404075320-75d898a42a94
 	golang.org/x/image v0.0.0-20190523035834-f03afa92d3ff
--- minio_writer.go
+++ minio_writer.go
@@ -1,19 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 
 	"github.com/minio/minio-go/v6"
+	uuid "github.com/nu7hatch/gouuid"
 )
 
-func writeImageToObjectStorage(scaledReader io.Reader, length int, contentType string, scale Scale, imageUpdate *ImageUpdate, config imageScalerConfig) error {
+func writeImageToObjectStorage(scaledReader io.Reader, length int, imageType string, scale Scale, imageUpdate *ImageUpdate, config imageScalerConfig) error {
 	var minioOpts minio.PutObjectOptions
+	minioOpts.ContentType = fmt.Sprintf("image/%s", imageType)
+	imageUUID, err := uuid.NewV4()
+	if err != nil {
+		log.Printf("error while creating image UUID: %v", err)
+		return err
+	}
 	minioClient, err := minio.New(config.minioURL, config.minioAccessKey, config.minioSecret, true)
 	if err != nil {
 		log.Printf("error while creating min.io client: %v", err)
 		return err
 	}
 	minioClient.PutObject(config.minioBucketName, "name", scaledReader, -1, minioOpts)
+
+	imageUpdate.ImageUUID = imageUUID.String()
+	imageUpdate.URL = fmt.Sprintf("%s/%s/%s", config.minioURL, config.minioBucketName, imageUUID)
+	scaleString, err := scaleToString(scale) // things can go wrong here
+	if err != nil {
+		return err
+	}
+	imageUpdate.ImageScale = scaleString
+
 	return nil
 }
