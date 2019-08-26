2814c225259d0fd5b44fff04add9f7398b2130b3
Robin de Silva Jayasinghe <jayasinghe@synyx.de>
2019-08-26 22:55:12 +0200

    add structured configuration
    we want to make the target formats configurable. as we want to keep
    the code agnostic to the differently named target formats structured
    configuration is a way to achieve this. in the target datastructure
    the formats are kept in a map. I chose toml as the format since json
    is to verbose, properties are not expressive enough and yaml just sucks.

--- config.go
+++ config.go
@@ -11,7 +11,6 @@ type imageScalerConfig struct {
 	port                  int
 	username              string
 	password              string
-	filename              string
 	imageExchange         string
 	imageUpdateQueue      string
 	imageUpdateRoutingKey string
@@ -22,11 +21,16 @@ type imageScalerConfig struct {
 	minioSecret           string
 	minioBucketName       string
 	minioSecure           bool
+	scalingTarget         map[string]scalingTargetConf
+}
+type scalingTargetConf struct {
+	Factor string `toml:"factor"`
+	Width  int    `toml:"width"`
 }
 
 func readConfig() imageScalerConfig {
-	viper.SetConfigFile("config.properties")
-	viper.SetConfigType("properties")
+	viper.SetConfigFile("config.yml")
+	viper.SetConfigType("yaml")
 
 	//default values suitable for vanilla rabbitmq docker container
 	viper.SetDefault("rabbitmq.hostname", "localhost")
@@ -34,9 +38,9 @@ func readConfig() imageScalerConfig {
 	viper.SetDefault("rabbitmq.username", "guest")
 	viper.SetDefault("rabbitmq.password", "guest")
 	viper.SetDefault("rabbitmq.timeout", "5s")
-	viper.SetDefault("rabbitmq.image.exchange", "user.event")
-	viper.SetDefault("rabbitmq.image.update.queue", "user.image.url.updated.dev")
-	viper.SetDefault("rabbitmq.image.update.routingkey", "user.image.url.updated.#")
+	viper.SetDefault("rabbitmq.image-exchange", "user.event")
+	viper.SetDefault("rabbitmq.image-update-queue", "user.image.url.updated.dev")
+	viper.SetDefault("rabbitmq.image-update-routingkey", "user.image.url.updated.#")
 
 	//default values suitable for min.io docker container
 	viper.SetDefault("minio.url", "localhost:9000")
@@ -50,21 +54,26 @@ func readConfig() imageScalerConfig {
 	confErr := viper.ReadInConfig()
 	logOnError(confErr, "No configuration file loaded - using defaults")
 
+	var scalingTargets map[string]scalingTargetConf
+
+	sub := viper.Sub("target")
+	sub.Unmarshal(&scalingTargets)
+
 	return imageScalerConfig{
 		hostname:              viper.GetString("rabbitmq.hostname"),
 		port:                  viper.GetInt("rabbitmq.port"),
 		username:              viper.GetString("rabbitmq.username"),
 		password:              viper.GetString("rabbitmq.password"),
 		timeout:               viper.GetDuration("rabbitmq.timeout"),
-		filename:              viper.GetString("filename"),
-		imageExchange:         viper.GetString("rabbitmq.image.exchange"),
-		imageUpdateQueue:      viper.GetString("rabbitmq.image.update.queue"),
-		imageUpdateRoutingKey: viper.GetString("rabbitmq.image.update.routingkey"),
+		imageExchange:         viper.GetString("rabbitmq.image-exchange"),
+		imageUpdateQueue:      viper.GetString("rabbitmq.image-update-queue"),
+		imageUpdateRoutingKey: viper.GetString("rabbitmq.image-update-routingkey"),
 		minioURL:              viper.GetString("minio.url"),
 		minioExternalURL:      viper.GetString("minio.external-url"),
 		minioAccessKey:        viper.GetString("minio.accesskey"),
 		minioSecret:           viper.GetString("minio.secret"),
 		minioBucketName:       viper.GetString("minio.bucketname"),
 		minioSecure:           viper.GetBool("minio.secure"),
+		scalingTarget:         scalingTargets,
 	}
 }
